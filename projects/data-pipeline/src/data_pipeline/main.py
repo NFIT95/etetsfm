399a8901f19dd24505a1faf9b8929da3e6084cd8
Nicola Filosi <nicola_filosi@mckinsey.com>
2024-01-05 15:16:52 +0000

    add main.py

---
+++ projects/data-pipeline/src/data_pipeline/main.py
@@ -0,0 +1,10 @@
+"""Main entry point for data pipeline"""
+from data_pipeline import settings
+
+def main():
+    """Main entry point"""
+    print(f"FANTASTIC JOB {settings.sample_setting}")
+
+
+if __name__ == "__main__":
+    main()
