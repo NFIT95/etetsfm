0846c93d4068467b9c25448e551395084b4b6a3f
Nicola Filosi <nicola_filosi@mckinsey.com>
2024-01-09 10:27:10 +0000

    delete loader.py

--- projects/data-pipeline/data_pipeline/curator.py
+++ projects/data-pipeline/data_pipeline/curator.py
@@ -5,14 +5,14 @@ import pandas as pd
 
 from data_pipeline.extractor import extract_data_from_json_file
 
-def create_table_for_data_quarantine(json_file_name: str) -> pl.DataFrame:
+def create_flat_structure_for_data_quarantine(json_file_name: str) -> pl.DataFrame:
     """PyDocs"""
     quarantined_table = pl.DataFrame(extract_data_from_json_file(json_file_name)[1])
     
     return quarantined_table
 
 
-def create_table_for_data_curation(json_file_name: str) -> pl.DataFrame:
+def create_flat_structure_for_data_curation(json_file_name: str) -> pl.DataFrame:
     """PyDocs"""
     table = pl.DataFrame(extract_data_from_json_file(json_file_name)[0])
     
