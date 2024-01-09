600f43e9b3dff1a27dbe98510eb0aae335db01e2
Nicola Filosi <nicola_filosi@mckinsey.com>
2024-01-09 10:18:02 +0000

    add flat structures functions to curators

--- projects/data-pipeline/data_pipeline/main.py
+++ projects/data-pipeline/data_pipeline/main.py
@@ -1,9 +1,12 @@
 """Main entry point for data pipeline"""
 from data_pipeline import settings
+from data_pipeline.curator import create_table_for_data_curation
 
 
 def main():
     """Main entry point"""
+    
+    create_table_for_data_curation("sales")
 
     print(f"FANTASTIC JOB {settings.sample_setting}")
 
