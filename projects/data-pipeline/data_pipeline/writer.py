d42ec0e452c744932d7ecd8b7dbc30e750f42323
Nicola Filosi <nicola_filosi@mckinsey.com>
2024-01-09 20:29:30 +0000

    update writer.py

--- projects/data-pipeline/data_pipeline/writer.py
+++ projects/data-pipeline/data_pipeline/writer.py
@@ -2,16 +2,16 @@
 
 from datetime import datetime
 
-import polars as pl
+import pandas as pd
 
 
 def write_data_to_file(
-    flat_structure: pl.DataFrame, folder_name: str, file_name: str, file_type: str
+    flat_structure: pd.DataFrame, folder_name: str, file_name: str, file_type: str
 ) -> None:
     """PyDocs"""
     output_path = f"data/{folder_name}/{str(datetime.now())}_{file_name}.{file_type}"
     match file_type:
         case "csv":
-            flat_structure.write_csv(output_path)
+            flat_structure.to_csv(output_path)
         case "parquet":
-            flat_structure.write_parquet(output_path)
+            flat_structure.to_parquet(output_path)
