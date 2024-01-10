7312a7e7d2c2bb828cbd91d136169faa09834a30
Nicola Filosi <nicola_filosi@mckinsey.com>
2024-01-10 16:42:51 +0000

    update main.py

--- projects/data-pipeline/data_pipeline/main.py
+++ projects/data-pipeline/data_pipeline/main.py
@@ -1,12 +1,25 @@
 """Main entry point for data pipeline"""
 from data_pipeline import settings
-from data_pipeline.curator import create_table_for_data_curation
+from data_pipeline.checker import (
+    create_gx_datasources,
+    create_gx_expectations_suites,
+    create_gx_filesystem_context,
+)
+from data_pipeline.curator import materialize_curated_flat_structures
 
 
 def main():
     """Main entry point"""
-    
-    create_table_for_data_curation("sales")
+
+    context = create_gx_filesystem_context()
+    create_gx_expectations_suites(context, settings.expectation_suites_names)
+    create_gx_datasources(context, settings.data_source_names)
+    materialize_curated_flat_structures(
+        context,
+        settings.files_names,
+        settings.expectation_suites_names[0],
+        settings.data_source_names[0],
+    )
 
     print(f"FANTASTIC JOB {settings.sample_setting}")
 
