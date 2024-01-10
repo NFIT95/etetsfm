1ff6a28ffc42dde458b5a21a77035337ce13f2e0
Nicola Filosi <nicola_filosi@mckinsey.com>
2024-01-10 16:44:48 +0000

    update writer module

--- projects/data-pipeline/data_pipeline/writer.py
+++ projects/data-pipeline/data_pipeline/writer.py
@@ -4,12 +4,23 @@ from datetime import datetime
 
 import pandas as pd
 
+STORAGE_ROOT_FOLDER = "data"
+
 
 def write_data_to_file(
     flat_structure: pd.DataFrame, folder_name: str, file_name: str, file_type: str
 ) -> None:
-    """PyDocs"""
-    output_path = f"data/{folder_name}/{str(datetime.now())}_{file_name}.{file_type}"
+    """
+    Writes a pandas dataframe to a physical file, with the file type
+    being either csv or parquet
+
+    Args:
+        flat_structure (pd.DataFrame): pandas dataframe to be written to disk
+        folder_name (str): name of the folder where the file will be written
+        file_name (str): name of the file that will be written
+        file_type (str): type of file that will be written, either csv or parquet
+    """
+    output_path = f"{STORAGE_ROOT_FOLDER}/{folder_name}/{str(datetime.now())}_{file_name}.{file_type}"
     match file_type:
         case "csv":
             flat_structure.to_csv(output_path)
