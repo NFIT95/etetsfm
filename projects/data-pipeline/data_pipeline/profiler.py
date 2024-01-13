88c593f27f0f2292f0a35e23f52e3cf9e4f3b6e5
Nicola Filosi <nicola_filosi@mckinsey.com>
2024-01-13 10:51:17 +0000

    initialise profiler module

--- projects/data-pipeline/data_pipeline/profiler.py
+++ projects/data-pipeline/data_pipeline/profiler.py
@@ -1,1 +1,28 @@
 """Profiler module to generate data profiles of curated and clean data"""
+
+from datetime import datetime
+from pathlib import Path
+
+import polars as pl
+from ydata_profiling import ProfileReport
+
+from data_pipeline.params import DATA_ROOT_FOLDER
+
+
+def write_data_profile_report(flat_structure: pl.DataFrame, json_file_name: str) -> None:
+    """
+    Creates html data profiling report for an input polars dataframe
+    
+    Args:
+        flat_structure (pl.DataFrame): input polars dataframe
+        json_file_name (str): input json file name
+    """
+
+    # Convert from polars to pandas dataframe for ydata-profiling integration
+    pd_flat_structure = flat_structure.to_pandas()
+
+    profile = ProfileReport(pd_flat_structure, title=f"Profile Report of {json_file_name}")
+    output_path = Path(
+        f"{DATA_ROOT_FOLDER}/data_profiles/{str(datetime.now())}_{json_file_name}.html"
+    )
+    profile.to_file(output_file=output_path)
